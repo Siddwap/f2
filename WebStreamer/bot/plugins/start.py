a916dbed967328fe6131c2fd76496c94d1ddd181
Siddwap <58450503+Siddwap@users.noreply.github.com>
2021-10-18 10:41:40 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link. /n If Bot is not responding click on https://sid-file2link.herokuapp.com',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link. If Bot is not responding click on https://sid-file2link.herokuapp.com',
                   reply_markup=InlineKeyboardMarkup(
                       [[
                             InlineKeyboardButton(
