09a9174ef493735062b5fc59cfc4329a5364bcb8
Siddwap <58450503+Siddwap@users.noreply.github.com>
2022-11-12 20:59:16 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link. If Bot is not responding click on https://sid-file2link.herokuapp.com',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant Download and Stream link. You cand directly download any file or Watch online. ',
                   reply_markup=InlineKeyboardMarkup(
                       [[
                             InlineKeyboardButton(
