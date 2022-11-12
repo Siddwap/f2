ce015fef2c8655cac6b490c47352585ad7b32fa1
Siddwap <58450503+Siddwap@users.noreply.github.com>
2022-11-12 21:10:29 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant Download and Stream link. You cand directly download any file or Watch online. ',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, I am SD File Stream Bot. Send me any telegram file to get an instant Download and Stream link. You can directly download any file or Watch online. ',
                   reply_markup=InlineKeyboardMarkup(
                       [[
                             InlineKeyboardButton(
